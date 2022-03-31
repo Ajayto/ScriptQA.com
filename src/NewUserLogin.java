c523cc09f066894f78392d9f1dc0db37a6d3e7df
unknown <ajaytoradmal2009@gmail.com>
2022-03-31 08:20:29 +0530

    review changes

--- src/NewUserLogin.java
+++ src/NewUserLogin.java
@@ -1,9 +1,9 @@
-
+	
 public class NewUserLogin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     System.out.println("this is for add new user to system");
 	}
 
 }
