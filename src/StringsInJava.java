c39f60e3845baccf51a585d95d32195b3fcbd514
Ajayto <ajaytoradmal2009@gmail.com>
2022-04-04 07:46:31 +0530

    Update StringsInJava.java

--- src/StringsInJava.java
+++ src/StringsInJava.java
@@ -6,6 +6,7 @@ public class StringsInJava {
 		//StringsInJava sj= new StringsInJava();
 		String s=" ";  //
 		String s1="hello Java"; 
+		String s2="hello Java"; 
 		
 		System.out.println(s.isEmpty());
 		System.out.println(s.isBlank());
